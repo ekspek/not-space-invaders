9c2f03a8ec30f1b4857460f78ebb9720a8a1c607
ekspek <daniel.de.schiffart@gmail.com>
2019-03-07 17:06:37 +0000

    Added physics to player entity

--- entities/player.lua
+++ entities/player.lua
@@ -1,4 +1,5 @@
 local state = require 'state'
+local world = require 'world'
 
 return function(x,y)
 	local entity = {
@@ -8,36 +9,55 @@ return function(x,y)
 		w = 13 * 2,
 		h = 8 * 2,
 		speed = 300, -- pixels per second
+		alive = true,
 	}
 
 	entity.quads_death = {}
+	local death_frame = 0
+	
+	entity.body = love.physics.newBody(world, entity.x, entity.y, 'kinematic')
+	entity.shape = love.physics.newRectangleShape(entity.w, entity.h - 3 * 2)
+	entity.fixture = love.physics.newFixture(entity.body, entity.shape)
+	entity.fixture:setUserData(entity)
 
 	entity.load = function(self)
 		self.image = love.graphics.newImage("sprites/player.png")
 		self.image_death = love.graphics.newImage("sprites/player_death.png")
-		table.insert(self.quads_death, love.graphics.newQuad(0, 0, 13, 8, self.image_death:getDimensions()))
+		table.insert(self.quads_death, love.graphics.newQuad(-1, 0, 13, 8, self.image_death:getDimensions()))
 		table.insert(self.quads_death, love.graphics.newQuad(16, 0, 13, 8, self.image_death:getDimensions()))
 	end
 
 	entity.update = function(self, dt)
-		if state.player.left then
+		self.x, self.y = self.body:getWorldPoints(self.shape:getPoints())
+		
+		if state.player.left == state.player.right then
+			self.body:setLinearVelocity(0,0)
+		elseif state.player.left then
 			if self.x - self.speed * dt > 0 then
-				self.x = self.x - self.speed * dt
+				self.body:setLinearVelocity(-self.speed, 0)
 			else
 				self.x = 0
+				self.body:setLinearVelocity(0,0)
 			end
 		elseif state.player.right then
 			if self.x + self.speed * dt < love.graphics.getWidth() - self.w then
-				self.x = self.x + self.speed * dt
+				self.body:setLinearVelocity(self.speed, 0)
 			else
 				self.x = love.graphics.getWidth() - self.w
+				self.body:setLinearVelocity(0,0)
 			end
 		end
+		
+		death_frame = (death_frame + 12 * dt) % 2
 	end
 
 	entity.draw = function(self)
 		love.graphics.setColor(0,1,0,1)
-		love.graphics.draw(self.image, self.x, self.y, 0, 2, 2)
+		if not self.alive then
+			love.graphics.draw(self.image_death, self.quads_death[math.floor(death_frame) + 1], self.x, self.y - 3 + 2, nil, 2, 2)
+		else
+			love.graphics.draw(self.image, self.x, self.y - 3 * 2, 0, 2, 2)
+		end
 	end
 
 	return entity
--- state.lua
+++ state.lua
@@ -3,7 +3,7 @@ local state = {
 	invader = {},
 	frame = 0,
 	frame_double = 0,
-	pace = 5, -- Invader speed
+	pace = 1, -- Invader speed
 }
 
 state.player.left = false
@@ -17,6 +17,12 @@ state.invader.direction = 'right'
 state.update = function(self, dt)
 	self.frame_double = (self.frame_double + dt * self.pace) % 2
 	self.frame = math.floor(self.frame_double)
+	
+	if self.invader.count > 0 then
+		self.pace = 55 / self.invader.count
+	else
+		self.pace = 1
+	end
 end
 
 return state
