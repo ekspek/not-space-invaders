c980b49d45cd13f397e331290fcaaf2fc0ff0015
ekspek <daniel.de.schiffart@gmail.com>
2019-03-07 05:04:46 +0000

    Added invader horde movement and border checks

--- entities/debug.lua
+++ entities/debug.lua
@@ -13,7 +13,8 @@ return function()
         love.graphics.setColor(1,1,1,1)
         love.graphics.print("fps = " .. self.fps, 0, love.graphics.getHeight() - 15)
         love.graphics.print("frame = " .. state.frame_double, 0, love.graphics.getHeight() - 30)
-        love.graphics.print("invaders = " .. state.invadercount, 0, love.graphics.getHeight() - 45)
+        love.graphics.print("invaders = " .. state.invader.count, 0, love.graphics.getHeight() - 45)
+        love.graphics.print("direction = " .. state.invader.direction, 0, love.graphics.getHeight() - 60)
     end
     
     return entity
