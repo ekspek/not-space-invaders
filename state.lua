81c8775d2b5f3dabb63b58f9ceeda0af81be3400
ekspek <daniel.de.schiffart@gmail.com>
2019-03-09 19:34:54 +0000

    Added invader bullet entity and spawning procedure

--- state.lua
+++ state.lua
@@ -10,18 +10,29 @@ state.player.left = false
 state.player.right = false
 state.player.firebuffer = false
 state.player.firehold = false
+state.player.alive = true
 
 state.invader.count = 0
 state.invader.direction = 'right'
 
 state.update = function(self, dt)
-	self.frame_double = (self.frame_double + dt * self.pace) % 2
-	self.frame = math.floor(self.frame_double)
-	
-	if self.invader.count > 0 then
-		self.pace = 55 / self.invader.count
+	if not self.player.alive then
+		self.pace = 0
+		self.frame = 0
+		self.frame_double = 0
+		self.player.left = false
+		self.player.right = false
+		self.player.firebuffer = false
+		self.player.firehold = false
 	else
-		self.pace = 1
+		self.frame_double = (self.frame_double + dt * self.pace) % 2
+		self.frame = math.floor(self.frame_double)
+		
+		if self.invader.count > 0 then
+			self.pace = 55 / self.invader.count
+		else
+			self.pace = 1
+		end
 	end
 end
 
