771b7b717bcd981f0662d538597292b7c5585dc5
Daniel de Schiffart <daniel.de.schiffart@gmail.com>
2019-04-21 18:28:18 +0200

    Change invader to hump class

--- entities/player.lua
+++ entities/player.lua
@@ -1,14 +1,15 @@
 local Class = require 'libs.hump.class'
+local Entity = require 'entity'
 
 local state = require 'state'
 local world = require 'world'
-local Entity = require 'entity'
 
 local Player = Class{ __includes = Entity }
 
 function Player:init(x, y)
 	Entity:init()
 
+	self.id = 'player'
 	self.x = x
 	self.y = y
 	self.w = 13 * 2
@@ -55,8 +56,8 @@ function Player:update(dt)
 	end
 
 	if not state.player.alive then
-		death_frame = (death_frame + 12 * dt) % 2
-		death_timer = death_timer + dt
+		self.death_frame = (self.death_frame + 12 * dt) % 2
+		self.death_timer = self.death_timer + dt
 	end
 end
 
@@ -64,7 +65,7 @@ function Player:draw()
 	love.graphics.setColor(0,1,0,1)
 	if not state.player.alive then
 		if death_timer < 1 then
-			love.graphics.draw(self.image_death, self.quads_death[math.floor(death_frame) + 1], self.x, self.y - 3 * 2, nil, 2, 2)
+			love.graphics.draw(self.image_death, self.quads_death[math.floor(self.death_frame) + 1], self.x, self.y - 3 * 2, nil, 2, 2)
 		end
 	else
 		love.graphics.draw(self.image, self.x, self.y - 3 * 2, 0, 2, 2)
@@ -80,82 +81,3 @@ function Player:postSolve(self, id)
 end
 
 return Player
-
---[[
-return function(x,y)
-	local entity = {
-		id = 'player',
-		x = x,
-		y = y,
-		w = 13 * 2,
-		h = 8 * 2,
-		speed = 100, -- pixels per second
-		alive = true,
-		margin = 2,
-	}
-
-	entity.quads_death = {}
-	local death_frame = 0
-	local death_timer = 0
-
-	entity.body = love.physics.newBody(world, entity.x, entity.y, 'kinematic')
-	entity.shape = love.physics.newRectangleShape(entity.w, entity.h - 3 * 2)
-	entity.fixture = love.physics.newFixture(entity.body, entity.shape)
-	entity.fixture:setUserData(entity)
-
-	entity.load = function(self)
-		self.image = love.graphics.newImage("sprites/player.png")
-		self.image_death = love.graphics.newImage("sprites/player_death.png")
-		table.insert(self.quads_death, love.graphics.newQuad(-1, 0, 13, 8, self.image_death:getDimensions()))
-		table.insert(self.quads_death, love.graphics.newQuad(16, 0, 13, 8, self.image_death:getDimensions()))
-
-		self.sound_death = love.audio.newSource("sfx/explosion.wav", "static")
-	end
-
-	entity.update = function(self, dt)
-		self.x, self.y = self.body:getWorldPoints(self.shape:getPoints())
-
-		if state.player.left == state.player.right or state.frozen then
-			self.body:setLinearVelocity(0,0)
-		elseif state.player.left then
-			if self.x - self.speed * dt > self.margin then
-				self.body:setLinearVelocity(-self.speed, 0)
-			else
-				self.body:setLinearVelocity(0,0)
-			end
-		elseif state.player.right then
-			if self.x + self.speed * dt < love.graphics.getWidth() - self.w - self.margin then
-				self.body:setLinearVelocity(self.speed, 0)
-			else
-				self.body:setLinearVelocity(0,0)
-			end
-		end
-
-		if not state.player.alive then
-			death_frame = (death_frame + 12 * dt) % 2
-			death_timer = death_timer + dt
-		end
-	end
-
-	entity.draw = function(self)
-		love.graphics.setColor(0,1,0,1)
-		if not state.player.alive then
-			if death_timer < 1 then
-				love.graphics.draw(self.image_death, self.quads_death[math.floor(death_frame) + 1], self.x, self.y - 3 * 2, nil, 2, 2)
-			end
-		else
-			love.graphics.draw(self.image, self.x, self.y - 3 * 2, 0, 2, 2)
-			death_timer = 0
-		end
-	end
-	
-	entity.postSolve = function(self, id)
-		if id == 'bullet_invader' or id == 'bullet' then
-			self.sound_death:play()
-			state.player.alive = false
-		end
-	end
-
-	return entity
-end
---]]
