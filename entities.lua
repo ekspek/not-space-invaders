c8292bdc1b9bca27815610f43ffe73cfb95ba1bc
Daniel de Schiffart <daniel.de.schiffart@gmail.com>
2019-04-21 18:51:09 +0200

    Change border entities to hump classes

--- entities.lua
+++ entities.lua
@@ -1,75 +1,66 @@
-local bullet = require 'entities.bullet'
 local bullet_invader = require 'entities.bullet_invader'
-local border = require 'entities.border'
-local border_bottom = require 'entities.border_bottom'
-local border_top = require 'entities.border_top'
 local gameover = require 'entities.gameover'
 local debug = require 'entities.debug'
 
 local Player = require 'entities.player'
 local Invader = require 'entities.invader'
 local Barrier = require 'entities.barrier'
+local Border = require 'entities.border'
+local Border_bottom = require 'entities.border_bottom'
+local Border_top = require 'entities.border_top'
+local Bullet = require 'entities.bullet'
 
 local entities = {
-	--[[
-	border(-10, 0, 10, love.graphics.getHeight()),
-	border(love.graphics.getWidth(), 0, 10, love.graphics.getHeight()),
-	border_top(0,0),
-	border_bottom(0, love.graphics.getHeight() - 30),
+	Border(-10, 0, 10, love.graphics.getHeight()),
+	Border(love.graphics.getWidth(), 0, 10, love.graphics.getHeight()),
+	Border_top(0,0),
+	Border_bottom(0, love.graphics.getHeight() - 30),
 
 	--debug(),
-	--]]
-	Invader(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 2),
 	Player(love.graphics.getWidth() / 2 - 16, love.graphics.getHeight() * 0.9),
 }
 
-for i = 1,4 do
-	table.insert(entities, Barrier((love.graphics.getWidth() / 5) * i, love.graphics.getHeight() * 0.8))
-end
-
---[[
 for i = 1,11 do
-	table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + 30, 3))
+	table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + 30, 3))
 end
 
 for j = 2,3 do
 	for i = 1,11 do
-		table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 2))
+		table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 2))
 	end
 end
 
 for j = 4,5 do
 	for i = 1,11 do
-		table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 1))
+		table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 1))
 	end
+end
+
+for i = 1,4 do
+	table.insert(entities, Barrier((love.graphics.getWidth() / 5) * i, love.graphics.getHeight() * 0.8))
 end
 
 entities.respawnInvaders = function(self, dt)
 	for i = 1,11 do
-		table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + 30, 3))
+		table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + 30, 3))
 	end
 	
 	for j = 2,3 do
 		for i = 1,11 do
-			table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 2))
+			table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 2))
 		end
 	end
 	
 	for j = 4,5 do
 		for i = 1,11 do
-			table.insert(entities, invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 1))
+			table.insert(entities, Invader((20 + ((love.graphics.getWidth() - 40) / 11) * i) - 16, 100 + j * 30, 1))
 		end
 	end
 
 	for i = 1,4 do
-		table.insert(entities, barrier((love.graphics.getWidth() / 5) * i, love.graphics.getHeight() * 0.8))
-	end
-
-	for _, entity in ipairs(entities) do
-		if entity.load then entity:load() end
+		table.insert(entities, Barrier((love.graphics.getWidth() / 5) * i, love.graphics.getHeight() * 0.8))
 	end
 end
---]]
 
 entities.update = function(self, dt)
 end
--- entities/border.lua
+++ entities/border.lua
@@ -1,21 +1,26 @@
+local Class = require 'libs.hump.class'
+local Entity = require 'entity'
+
 local state = require 'state'
 local world = require 'world'
 
-return function(x,y,w,h)
-	local entity = {
-		id = 'border',
-		x = x,
-		y = y,
-		w = w,
-		h = h,
-	}
+Border = Class{ __includes = Entity }
 
-	entity.body = love.physics.newBody(world, entity.x + (entity.w / 2), entity.y + (entity.h / 2), 'static')
-	entity.shape = love.physics.newRectangleShape(entity.w, entity.h)
-	entity.fixture = love.physics.newFixture(entity.body, entity.shape)
-	entity.fixture:setRestitution(2)
-	entity.fixture:setFriction(1)
-	entity.fixture:setUserData(entity)
+function Border:init(x,y,w,h)
+	Entity:init()
 
-	return entity
+	self.id = 'border'
+	self.x = x
+	self.y = y
+	self.w = w
+	self.h = h
+
+	self.body = love.physics.newBody(world, self.x + (self.w / 2), self.y + (self.h / 2), 'static')
+	self.shape = love.physics.newRectangleShape(self.w, self.h)
+	self.fixture = love.physics.newFixture(self.body, self.shape)
+	self.fixture:setRestitution(2)
+	self.fixture:setFriction(1)
+	self.fixture:setUserData(self)
 end
+
+return Border
